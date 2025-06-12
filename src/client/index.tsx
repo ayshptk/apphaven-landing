8f7c77019166f42037f71040bcac5b57d1b9950c
ayush <62694274+ayshptk@users.noreply.github.com>
2025-06-12 10:21:01 +0530

    Update index.tsx

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -90,27 +90,13 @@ function App() {
 
   return (
     <div className="App">
-      <h1>Where's everyone at?</h1>
-      {counter !== 0 ? (
-        <p>
-          <b>{counter}</b> {counter === 1 ? "person" : "people"} connected.
-        </p>
-      ) : (
-        <p>&nbsp;</p>
-      )}
+      <h1>Apphaven</h1>
 
       {/* The canvas where we'll render the globe */}
       <canvas
         ref={canvasRef as LegacyRef<HTMLCanvasElement>}
         style={{ width: 400, height: 400, maxWidth: "100%", aspectRatio: 1 }}
       />
-
-      {/* Let's give some credit */}
-      <p>
-        Powered by <a href="https://cobe.vercel.app/">🌏 Cobe</a>,{" "}
-        <a href="https://www.npmjs.com/package/phenomenon">Phenomenon</a> and{" "}
-        <a href="https://npmjs.com/package/partyserver/">🎈 PartyServer</a>
-      </p>
     </div>
   );
 }
